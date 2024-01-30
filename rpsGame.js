94a64eae8c73a0288fdb73f04d4deb2948774fff
Vaishnavi  Rana <vaishnavirana5@gmail.com>
2024-01-30 12:38:23 +0530

    rps game

---
+++ rpsGame.js
@@ -0,0 +1,75 @@
+let userscore = 0 ;
+let compscore = 0 ;
+
+const choices =  document.querySelectorAll(".choice");
+ let msg = document.querySelector("#msg");
+ const userScorePara = document.querySelector("#user-score");
+ const compScorePara = document.querySelector("#comp-score");
+const gencompchoice = () => {
+	const options = ["rock" ,"paper","scissors"]
+   const randoms  = Math.floor(Math.random() * 3);
+   return options[randoms];
+}
+const drawGame = () =>{
+
+	// @ts-ignore
+	msg.innerText = "It's a DRAW. Play again";
+	// @ts-ignore
+	msg.style.backgroundColor = "yellow"
+
+}
+
+const showWinner = (userWin,userchoice,compChoice) =>{
+	if (userWin) {
+		userscore++
+		// @ts-ignore
+		userScorePara.innerText =userscore ;
+		console.log("You Win");
+		// @ts-ignore
+		msg.innerText = `Yayy..You Win..! Your ${userchoice} beats ${compChoice}`;
+		// @ts-ignore
+		msg.style.backgroundColor = "green"
+	}else {
+	
+		compscore++
+		// @ts-ignore
+		compScorePara.innerText = compscore ;
+		// @ts-ignore
+		msg.innerText = `:( You lost ${compChoice} beats Your ${userchoice}...`;
+		// @ts-ignore
+		msg.style.backgroundColor = "red";
+
+	}
+}
+
+const playGame = (userchoice) =>{
+	console.log("User Choice = " ,userchoice );
+	//Generate computer choice
+	const compChoice =gencompchoice();
+	console.log("Computer Choice = " ,compChoice );
+
+	if (userchoice == compChoice ) {
+		drawGame() ;
+	}else{
+		let userwin = true ;
+		if(userchoice == "rock"){
+			compChoice === 'paper' ?false : true ;
+
+		}else if(userchoice === "paper"){
+			userwin = compChoice === "scissors" ? false :true ;
+
+		}else {
+			compChoice === "rock" ? false :true ;
+		}
+		showWinner(userwin,userchoice,compChoice);
+	}
+}
+
+choices.forEach((choice) => {
+	console.log(choice);
+	 choice.addEventListener("click",()=>{
+		
+  const userChoice =choice.getAttribute("id");
+  playGame(userChoice)
+	 })
+})
